b252317281e61cea27f5745cf4ba88e6826d4233
xkicken <61647663+xkicken@users.noreply.github.com>
2024-10-04 15:28:10 +1300

    edit CorsConfig.java

--- src/main/java/xyz/xkicken/Grocery/config/CorsConfig.java
+++ src/main/java/xyz/xkicken/Grocery/config/CorsConfig.java
@@ -14,11 +14,8 @@ public class CorsConfig {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:63343")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                registry.addMapping("/**")
+                        .allowedOrigins("*");
             }
         };
     }
